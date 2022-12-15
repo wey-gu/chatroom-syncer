2f7a742a13a2f9d94899d05481bfbf226be986f3
Wey Gu <weyl.gu@gmail.com>
2022-12-15 15:13:35 +0000

    feat: support avatar with random emoji
    partially implement: #2
    
    also add aiohttp as dep due to async slack client

--- src/chatroom_syncer/room_syncer.py
+++ src/chatroom_syncer/room_syncer.py
@@ -1,8 +1,11 @@
 from wechaty import Wechaty, Message, FileBox
 from wechaty_puppet import MessageType
 
-
-from .utils import format_msg_text, send_slack_message, prepare_for_configuration
+from .utils import (
+    format_msg_text,
+    send_slack_message,
+    prepare_for_configuration,
+    get_emoji)
 
 
 class RoomSyncBot(Wechaty):
@@ -14,6 +17,7 @@ class RoomSyncBot(Wechaty):
         super().__init__()
         self.slack_token = self._config["slack_token"]
         self.group_to_channel = self._config["group_channel_mapping"]
+        self.avatar_cache = {}
 
     async def on_message(self, msg: Message) -> None:
         room = msg.room()
@@ -24,13 +28,22 @@ class RoomSyncBot(Wechaty):
                 if msg.type() == MessageType.MESSAGE_TYPE_TEXT:
                     text = format_msg_text(msg.text())
                     if text:
-                        username = msg.talker().name
-                        # TBD: get avatar from wechaty and cache it
+                        contact = msg.talker()
+                        username = contact.name
+                        if self._config["enable_avatar"]:
+                            if username not in self.avatar_cache:
+                                avatar_emoji = get_emoji()
+                                self.avatar_cache[username] = avatar_emoji
+                            else:
+                                avatar_emoji = self.avatar_cache[username]
+                        else:
+                            avatar_emoji = None
                         await send_slack_message(
                             text=text,
                             channel=self.group_to_channel[topic],
                             username=username,
-                            slack_token=self.slack_token)
+                            slack_token=self.slack_token,
+                            icon_emoji=avatar_emoji)
 
                 if msg.type() == MessageType.MESSAGE_TYPE_IMAGE:
                     # TBD: send image to slack
